016bb5ab679a434e9ec8e7ea03efafb6351446ed
otaviopetry <otaviopetry@gmail.com>
2020-07-31 03:10:57 -0300

    chore: Screen data structure

--- src/pages/DetailsScreen/index.tsx
+++ src/pages/DetailsScreen/index.tsx
@@ -1,11 +1,97 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, Summary, CasesContainer, Cases } from './styles';
 
 const DetailsScreen: React.FC = () => {
   return (
     <Container>
-      <h1>Details Screen</h1>
+      <Summary>
+        <h1>Summary</h1>
+      </Summary>
+      <CasesContainer>
+        <Cases>
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+
+          <Link to="/case">
+            <span>Urgent</span>
+            <div className="case-info">
+              <span className="timestamp">08:05:35PM</span>
+              <p>At home</p>
+            </div>
+          </Link>
+        </Cases>
+      </CasesContainer>
     </Container>
   );
 };
--- src/pages/DetailsScreen/styles.ts
+++ src/pages/DetailsScreen/styles.ts
@@ -2,7 +2,51 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  width: 100%;
   min-height: 100vh;
+  align-items: stretch;
+  justify-content: center;
+`;
+
+export const Summary = styled.div`
+  background: #24423a;
+  width: 20%;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  display: flex;
   align-items: center;
   justify-content: center;
+  color: #fefefe;
+`;
+
+export const CasesContainer = styled.div`
+  background: #fefefe;
+  width: 80%;
+  margin-left: 20%;
+  padding-top: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #333;
+`;
+
+export const Cases = styled.div`
+  width: 720px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+
+  a {
+    background: #eaeaea;
+    border-radius: 5px;
+    flex-basis: 48%;
+    padding: 24px;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    margin-bottom: 16px;
+  }
 `;
