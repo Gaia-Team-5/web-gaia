059469bbc41fd28e91fc4170b47bccc5fc6dab9e
Otavio Petry <otaviopetry@gmail.com>
2020-07-31 14:55:59 -0300

    chore: CaseContainer header

--- src/pages/CaseScreen/index.tsx
+++ src/pages/CaseScreen/index.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import api from '../../services/api';
 
-const CaseScreen: React.FC = () => {
+import {
+  Container,
+  CaseContainer,
+  InfoContainer,
+  MapContainer,
+} from './styles';
+
+interface Case {
+  id: number;
+  category: 'isolated' | 'injured' | 'death risk' | 'other';
+  gaia_report: string;
+  risk: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  created_at: string;
+  status: 'pending' | 'ongoing';
+}
+
+const CaseScreen: React.FC = navigation => {
+  const [emergencyCases, setCases] = useState<Case[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/get-cases')
+      .then(response => {
+        setCases(response.data);
+      })
+      .catch(err => console.log(err));
+  }, [navigation]);
+
   return (
     <Container>
-      <h1>Case Screen</h1>
+      <CaseContainer>
+        <header>
+          <span className="risk">urgent</span>
+          <span className="status">ongoing</span>
+        </header>
+        <section>
+          <InfoContainer>
+            <h3>Case #1</h3>
+            <span className="category">ISOLATED PERSON</span>
+
+            <strong>Protocol generated at:</strong>
+            <p>14:17:05 - 31/07/20</p>
+
+            <strong>gaia Assistant report:</strong>
+            <p>
+              Family isolated at house, can’t get out. Risk of water getting in.
+            </p>
+
+            <strong>Status:</strong>
+            <p>Rescue team is on its way.</p>
+          </InfoContainer>
+          <MapContainer>
+            <h1>Mapa</h1>
+          </MapContainer>
+        </section>
+      </CaseContainer>
     </Container>
   );
 };
--- src/pages/CaseScreen/styles.ts
+++ src/pages/CaseScreen/styles.ts
@@ -6,3 +6,56 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
+export const CaseContainer = styled.div`
+  width: 800px;
+  background: #eaeaea;
+  color: #333;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    background: #24423a;
+    color: #fff;
+
+    display: flex;
+    justify-content: space-between;
+
+    .risk {
+      background: #c14545;
+
+      padding: 16px;
+
+      margin-left: 32px;
+    }
+
+    .status {
+      font-weight: bold;
+      padding: 16px;
+
+      margin-right: 16px;
+    }
+  }
+
+  section {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 32px;
+
+  width: 48%;
+`;
+
+export const MapContainer = styled.div`
+  display: flex;
+
+  width: 52%;
+`;
